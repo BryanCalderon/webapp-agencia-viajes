13a37eb523f144c9b73bee1ca0c17e5730f10640
Bryan Calderon <bcalderon@retailcompass.com>
2021-06-22 21:57:05 -0500

    Se anexa pagina 404, se ajusta test

---
+++ src/app/components/error/error.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-error',
+  templateUrl: './error.component.html',
+  styleUrls: ['./error.component.css']
+})
+export class ErrorComponent {
+
+  @Input() message;
+  @Input() status;
+
+  constructor() {
+  }
+
+}
